fc0f77274bdaee9accd536ac6270daa11ec78968
Jonathan Pumares <jepc1491@gmail.com>
2015-09-08 00:10:08 -0500

    Definición del modelo Ponente.js junto con su controlador PonenteController.js

---
+++ scorec/api/controllers/PonenteController.js
@@ -0,0 +1,11 @@
+/**
+ * PonenteController
+ *
+ * @description :: Server-side logic for managing ponentes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ scorec/api/models/Ponente.js
@@ -0,0 +1,35 @@
+/**
+* Ponente.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	tableName: 'ponentes',
+	attributes: {
+		idPonente: {
+			type: 'integer',
+	  		autoIncrement: true,
+	  		primaryKey: true,
+	  		required: true,
+	  		unique: true,
+	  		columnName: 'id_ponente'
+		},
+		nombre: {
+			type: 'string',
+			size: 80,
+			required: true,
+			columnName: 'nombre'
+		},
+		empresa: {
+			type: 'string',
+			size: 80,
+			required: true,
+			columnName: 'empresa'
+		}
+
+
+	}
+};
+
