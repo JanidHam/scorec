cdac6924e55c0ab2388c72d06183ee2854a893d2
Jonathan Pumares <jepc1491@gmail.com>
2015-09-07 23:50:08 -0500

    Definición del modelo Asistente.js junto con su controlador AsistenteController.js

---
+++ scorec/api/controllers/AsistenteController.js
@@ -0,0 +1,11 @@
+/**
+ * AsistenteController
+ *
+ * @description :: Server-side logic for managing asistentes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ scorec/api/models/Asistente.js
@@ -0,0 +1,59 @@
+/**
+* Asistente.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	tableName: 'asistentes',
+  	attributes: {
+  		idAsistente: {
+	  		type: 'integer',
+	  		autoIncrement: true,
+	  		primaryKey: true,
+	  		required: true,
+	  		unique: true,
+	  		columnName: 'id_asistente'
+	  	},
+	  	apellidos: {
+	  		type: 'string',
+	  		size: 40,
+	  		required: true,
+	  		columnName: 'apellidos'
+	  	},
+	  	nombre: {
+	  		type: 'string',
+	  		size: 40,
+	  		required: true,
+	  		columnName: 'apellidos'
+	  	},
+	  	telefono: {
+	  		type: 'string',
+	  		size: 10,
+	  		columnName: 'telefono'
+	  	},
+	  	email: {
+	  		type: 'email',
+	  		unique: true,
+	  		columnName: 'email'
+	  	},
+	  	direccion: {
+	  		type: 'strig',
+	  		size: 100,
+	  		columnName: 'direccion'
+	  	},
+	  	registrado: {
+	  		type: 'integer',
+	  		defaultsTo: 0,
+	  		columnName: 'registrado'
+	  	},
+	  	numAsistencias: {
+	  		type: 'integer',
+	  		defaultsTo: 0,
+	  		columnName: 'num_asistencias'
+	  	}
+
+  	}
+};
+
