de66460a32e205804e32ceabbc5ed75b9dfac934
Jonathan Pumares <jepc1491@gmail.com>
2015-09-08 00:39:25 -0500

    Definición del modelo Tema.js junto con su controlador TemaController.js

---
+++ scorec/api/models/Tema.js
@@ -0,0 +1,29 @@
+/**
+* Tema.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	tableName: 'temas',
+	attributes: {
+		idTema: {
+			type: 'integer',
+	  		autoIncrement: true,
+	  		primaryKey: true,
+	  		required: true,
+	  		unique: true,
+	  		columnName: 'id_tema'
+		},
+		nombre: {
+			type: 'string',
+			size: 150,
+			required: true,
+			columnName: 'nombre'
+		},
+
+
+	}
+};
+
